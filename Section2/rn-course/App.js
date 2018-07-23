7406273efd8d80f3e4e682f2e62b7cf44fe782c6
srichmond3000 <srichmond3000@googlemail.com>
2018-07-23 21:45:02 +0100

    Finished Lecture 27.

--- Section2/rn-course/App.js
+++ Section2/rn-course/App.js
@@ -3,14 +3,30 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   };
 
   placeNameChangedHander = (value) => {
     this.setState({ placeName: value });
   };
 
+  placeSubmitHandler = () => {
+    if(this.state.placeName.trim() === ''){
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
+
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
@@ -23,7 +39,11 @@ export default class App extends React.Component {
           <Button
             title="Add"
             style={styles.placeButton}
+            onPress={this.placeSubmitHandler}
           />
+        </View>
+        <View>
+          {placesOutput}
         </View>
       </View>
     );
