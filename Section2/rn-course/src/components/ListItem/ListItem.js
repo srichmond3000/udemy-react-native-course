8c3f9f90f1414527a87349bdd8813c5479dbb291
srichmond3000 <srichmond3000@googlemail.com>
2018-07-26 07:06:44 +0100

    Lecture 33 Adding static images.

--- Section2/rn-course/src/components/ListItem/ListItem.js
+++ Section2/rn-course/src/components/ListItem/ListItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const ListItem = (props) => (
   <TouchableOpacity onPress={props.onItemPressed}>
     <View style={styles.listItem}>
+      <Image
+        resizeMode="contain"
+        source={props.placeImage}
+        style={styles.placeImage} />
       <Text>{props.placeName}</Text>
     </View>
   </TouchableOpacity>
@@ -14,7 +18,14 @@ const styles = StyleSheet.create({
     width: "100%",
     marginBottom: 5,
     padding: 10,
-    backgroundColor: "#eee"
+    backgroundColor: "#eee",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  placeImage: {
+    marginRight: 8,
+    height: 30,
+    width: 30
   }
 });
 
