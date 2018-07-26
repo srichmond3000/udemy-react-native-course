6d1ca9ebf2d31db903d524124fa79d9197c3ec75
srichmond3000 <srichmond3000@googlemail.com>
2018-07-26 06:31:46 +0100

    Lecture 32 Rendering lists correctly

--- Section2/rn-course/App.js
+++ Section2/rn-course/App.js
@@ -12,16 +12,19 @@ export default class App extends React.Component {
   placeAddedHandler = (placeName) => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random(),
+          value: placeName
+        })
       };
     });
   };
 
-  placeDeletedHandler = (index) => {
+  placeDeletedHandler = (key) => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter((place) => {
+          return place.key !== key;
         })
       };
     });
--- Section2/rn-course/src/components/PlaceList/PlaceList.js
+++ Section2/rn-course/src/components/PlaceList/PlaceList.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
+  return (
+    <FlatList
+      style={styles.listContainer}
+      data={props.places}
+      renderItem={(info) => (
         <ListItem
-            key={i}
-            placeName={place}
-            onItemPressed={() => props.onItemDeleted(i)}
+          placeName={info.item.value}
+          onItemPressed={() => props.onItemDeleted(info.item.key)}
         />
-    ));
-    return (
-        <ScrollView style={styles.listContainer}>{placesOutput}</ScrollView>
-    );
+      )}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    listContainer: {
-        width: "100%"
-    }
+  listContainer: {
+    width: "100%"
+  }
 });
 
 export default placeList;
